0525fcdbde0951ab7230171a55260a6eccf5b91b
Reider745 <fhgfdf574@gmail.com>
2025-08-05 13:23:38 +0300

    fix: palette

--- src/main/java/cn/nukkit/block/customblock/CustomBlockUtil.java
+++ src/main/java/cn/nukkit/block/customblock/CustomBlockUtil.java
@@ -91,8 +91,6 @@ public class CustomBlockUtil {
             }
         }
 
-        System.out.println(statesBuilder.toString());
-
         NbtMap state = NbtMap.builder()
                 .putString("name", identifier)
                 .putCompound("states", statesBuilder.build())
