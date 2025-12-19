fb9d6fa339a45843a42b298024b115f7646a5ea4
SocialMoods <nikelev01@gmail.com>
2025-12-19 10:11:51 +0300

    feat: command blocks (WIP)
    * need to fix NPE after setting command and open command block inventory
    
    Co-authored-by: GamerPlay3975

--- src/main/java/cn/nukkit/block/BlockCommandBlockRepeating.java
+++ src/main/java/cn/nukkit/block/BlockCommandBlockRepeating.java
@@ -1,8 +1,6 @@
 package cn.nukkit.block;
 
-import cn.nukkit.item.Item;
-
-public class BlockCommandBlockRepeating extends BlockSolid {
+public class BlockCommandBlockRepeating extends BlockCommandBlock {
 
     @Override
     public int getId() {
@@ -10,32 +8,7 @@ public class BlockCommandBlockRepeating extends BlockSolid {
     }
 
     @Override
-    public double getHardness() {
-        return -1;
-    }
-
-    @Override
-    public double getResistance() {
-        return 18000000;
-    }
-
-    @Override
     public String getName() {
         return "Repeating Command Block";
-    }
-
-    @Override
-    public boolean isBreakable(Item item) {
-        return false;
-    }
-
-    @Override
-    public boolean canBePushed() {
-        return false;
-    }
-
-    @Override
-    public boolean canHarvestWithHand() {
-        return false;
     }
 }
