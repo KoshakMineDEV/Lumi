7ec98395dfb9fdc025b5f18f92ce4cd561843c6f
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2026-03-09 00:26:19 +0300

    fix: noteblock

--- src/main/java/cn/nukkit/block/BlockNoteblock.java
+++ src/main/java/cn/nukkit/block/BlockNoteblock.java
@@ -121,18 +121,6 @@ public class BlockNoteblock extends BlockSolid implements BlockEntityHolder<Bloc
             case JUNGLE_PLANKS:
             case ACACIA_PLANKS:
             case DARK_OAK_PLANKS:
-            case OAK_SLAB:
-            case SPRUCE_SLAB:
-            case BIRCH_SLAB:
-            case JUNGLE_SLAB:
-            case ACACIA_SLAB:
-            case DARK_OAK_SLAB:
-            case OAK_DOUBLE_SLAB:
-            case SPRUCE_DOUBLE_SLAB:
-            case BIRCH_DOUBLE_SLAB:
-            case JUNGLE_DOUBLE_SLAB:
-            case ACACIA_DOUBLE_SLAB:
-            case DARK_OAK_DOUBLE_SLAB:
             case WOOD_STAIRS:
             case SPRUCE_WOOD_STAIRS:
             case BIRCH_WOOD_STAIRS:
@@ -195,11 +183,6 @@ public class BlockNoteblock extends BlockSolid implements BlockEntityHolder<Bloc
             case NETHER_BRICK_BLOCK:
             case RED_NETHER_BRICK:
             case QUARTZ_BLOCK:
-            //TODO: Fix
-            //case DOUBLE_SLAB:
-            //case SLAB:
-            //case DOUBLE_RED_SANDSTONE_SLAB:
-            case RED_SANDSTONE_SLAB:
             case COBBLE_STAIRS:
             case BRICK_STAIRS:
             case STONE_BRICK_STAIRS:
